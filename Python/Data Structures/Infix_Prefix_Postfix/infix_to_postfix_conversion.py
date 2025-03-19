40d2b71e0b4a237a305b7827bea960f3064fc9d7
Abhinav <abhiasthana76@gmail.com>
2025-03-19 20:48:15 +0530

    Infix to Postfix Conversion

--- Python/Data Structures/Infix_Prefix_Postfix/infix_to_postfix_conversion.py
+++ Python/Data Structures/Infix_Prefix_Postfix/infix_to_postfix_conversion.py
@@ -1,4 +1,6 @@
 # Infix to Postfix Conversion
+
+# Operator precedence
 def precedence(char: str):
     if char == "+" or char == "-":
         return 1  # lowest precedency
@@ -10,40 +12,53 @@ def precedence(char: str):
         return -1
 
 
+# conversion
 def infixToPostfix(equation: str):
-    TOP = -1
     eq = []  # stack
     result = ""  # resulting conversion
 
     try:
         for i in equation:
+            # if the character is operand, append it to result
             if i.isalnum():
                 result += i
 
+            # if incoming symbol is '(' push it onto stack
             elif i == "(":
                 eq.append(i)
-                TOP += 1
 
+            # if incoming symbol is ')', pop the stack & append the popped symbol to result
+            # until left paranthesis '(' is found
             elif i == ")":
-                while eq[TOP] != "(":
+                while eq[-1] != "(":
                     result += eq.pop()
-                    TOP -= 1
                 eq.pop()
+
+            # check precedence
             else:
                 while eq and (
-                    precedence(i) < precedence(eq[TOP])
-                    or precedence(i) == precedence(eq[TOP])
+                    precedence(i) < precedence(eq[-1])
+                    or precedence(i) == precedence(eq[-1])
                 ):
                     result += eq.pop()
-                    TOP -= 1
                 eq.append(i)
+
+        # pop the remaining symbols from stack and append them to result
+        while eq:
+            result += eq.pop()
+
+        print("Equation: ", equation)
+        print("Infix to postfix conversion: ", result)
+        print()
 
     except Exception as e:
         print("An Exception Occurred: ", e.__repr__())
 
-    print(result)
-    print(eq)
 
-
-equation = "A-B/C*D+E"
-infixToPostfix(equation)
+if __name__ == "__main__":
+    equation1 = "A-(B/C)*D+E"
+    equation2 = "K+L-M*N+(O^P)*W/U/V*T+Q^J^A"
+    equation3 = "A-B+(M^N)*(O+P)-Q/R^S*T+Z"
+    infixToPostfix(equation1)
+    infixToPostfix(equation2)
+    infixToPostfix(equation3)
