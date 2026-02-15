3fd116be41ca88e77086fa4b00974595c62367ce
Abhinav <abhiasthana76@gmail.com>
2026-02-15 12:31:18 +0530

    feat(collections): ArrayList

---
+++ Java/src/com/corejava/notes/collections/Arraylist.java
@@ -0,0 +1,82 @@
+package com.corejava.notes.collections;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Collections;
+
+public class Arraylist {
+    public static void main(String[] args) {
+        // initialize ArrayList of integers
+        ArrayList<Integer> list = new ArrayList<>();
+
+        // Adding elements to the end of the list
+        list.add(400);
+        list.add(2);
+        list.add(3);
+
+        // Adding element at specific index
+        list.add(2, 50);
+        System.out.println(list);
+
+        // access elements
+        System.out.println(list.get(2));
+
+        // get size of the list
+        System.out.println(list.size());
+
+        // traverse
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println(list.get(i));
+        }
+
+        // traverse over each element
+        for (int x: list) {
+            System.out.println(x);
+        }
+
+        // we can print list without using loop
+        // as list extends one of the classes implementing
+        // toString() function
+        System.out.println(list);
+
+        // check for elements
+        System.out.println(list.contains(5));
+        System.out.println(list.contains(10));
+
+        // remove element using index
+        list.remove(2);
+        System.out.println(list);
+
+        // remove elements using value
+        // for integers, the method by default takes the
+        // parameter to index
+        // list.remove(400);
+        // so we need convert it to the wrapper class
+        list.remove(Integer.valueOf(400));
+        System.out.println(list);
+
+        // replace an element
+        list.set(2, 100);
+        System.out.println(list);
+
+        // initialize using Arrays
+        // add or removal not allowed as this; unmodifiable
+        // creates a private fixed size array list
+        String[] array = {"Apple", "Banana", "Cherry"};
+        List<String> list1 = Arrays.asList(array);
+        System.out.println(list1.getClass().getName());
+        list1.add("Mango"); // throws exception
+
+        // make unmodifiable list modifiable
+        List<String> list2 = new ArrayList<>(list1);
+        list2.add("Mango");
+
+        // create list of immutable objects
+        List<Integer> list3 = List.of(1, 2, 3, 4, 5);
+        list3.set(1, 5); // throws an exception
+
+        // sort a ArrayList
+        Collections.sort(list);
+        System.out.println(list);
+    }
+}
